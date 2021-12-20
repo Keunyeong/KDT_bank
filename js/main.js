6ae81a8c1ef40e4862f42c082cb76d823a7ce7b8
keunyeong <91kyeong@gmail.com>
2021-12-20 14:38:16 +0900

    feat: Completed main Style.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const accountHistoryUrl = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f6e4d3d3-c52c-4ea8-b665-968a3b17c5ea/bank.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211217%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211217T084316Z&X-Amz-Expires=86400&X-Amz-Signature=f53f385c302c470c8f93ab8c115f90f534961bd8c37cd103f6c34f8c73d56ea3&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22bank.json%22&x-id=GetObject";
+const accountHistoryUrl = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f6e4d3d3-c52c-4ea8-b665-968a3b17c5ea/bank.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211220%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211220T041540Z&X-Amz-Expires=86400&X-Amz-Signature=730c6e0812549d1772458c47dd88109cc6877187aa6ef3408ea3b9eb6bd32f90&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22bank.json%22&x-id=GetObject";
 const sectionEle = document.querySelector(".contents__history__list");
 const extensionBtn = document.querySelector(".contents__sizeup > a")
 const phoneElem = document.querySelector(".phone");
@@ -45,6 +45,7 @@ function accountHistoryUpload(url){
     };
     const set = new Set(dateArr);
     const newDateArr = [...set];
+
     let dateFindIndexArr = [];
     for(let i=0; i<newDateArr.length; i++){
       dateFindIndexArr.push(data.bankList.findIndex(n=>n.date==newDateArr[i]));
